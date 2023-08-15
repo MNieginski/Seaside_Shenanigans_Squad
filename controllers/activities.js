0e23b90654a2729b2e9ca25f0f6656d3efe1a574
laurhimms <lauriehimmel1@gmail.com>
2023-08-15 13:17:12 -0700

    chatgpt is working

--- controllers/activities.js
+++ controllers/activities.js
@@ -1,12 +1,19 @@
 const Vacation = require("../models/vacation")
+const { Configuration, OpenAIApi } = require("openai");
 
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 
 
 
 module.exports = {
     create,
-    delete: deleteActivity
+    delete: deleteActivity,
+    getResponse,
+    showActivity
 }
 
 async function create(req, res) {
@@ -32,5 +39,47 @@ async function deleteActivity(req, res) {
     vacation.activities.splice(idx, 1)
     await vacation.save()
     // await Activity.deleteOne({_id: req.params.aid})
+    res.redirect(`/vacations/${vacation._id}`)
+}
+
+
+async function getResponse(req,res, next) {
+    const thisVacation = await Vacation.findById(req.params.id)
+
+
+    const response = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [
+            {
+                role: 'system',
+                content: `I will be in ${thisVacation.location} between ${thisVacation.departure} and ${thisVacation.arrival}, suggest one activity for me to do. Create an Activity Title (3 words or less) and a Description (3 sentences or less), but do not include the words "Activity Title" or "Description".`,
+            },
+        ],
+        temperature: 0,
+          max_tokens: 500,
+          top_p: 1,
+          frequency_penalty: 0,
+          presence_penalty: 0,
+    });
+    const message = response.data.choices[0].message
+    const activityContent = message.content
+    const splitEm = activityContent.split(': ')
+    const newTitle = splitEm[0]
+    const newActivity = splitEm[1]
+    const activityObj = {name: newTitle, description: newActivity}
+    showActivity(req, res, next, activityObj);
+}
+
+async function showActivity(req, res, next, activityObj) {
+    const vacation = await Vacation.findById(req.params.id)
+
+    vacation.activities.push(activityObj)
+
+    try {
+        await vacation.save()
+    } catch (err) {
+        console.log(err)
+    }
+
     res.redirect(`/vacations/${vacation._id}`)
 }
--- routes/activities.js
+++ routes/activities.js
@@ -16,26 +16,11 @@ router.post('/vacations/:id/activities', activityCtrl.create)
 
 router.delete('/vacations/:vid/:aid', activityCtrl.delete)
 
-const userInput = 'Success Tips';
+router.get('/vacations/:id/activities/new',activityCtrl.getResponse)
 
-const getResponse = async () => {
-    const response = await openai.createChatCompletion({
-        model: 'gpt-3.5-turbo',
-        messages: [
-            {
-                role: 'system',
-                content: `I will be in Spain between October 12, 2023 and October 25 2023, suggest one activity for me to do. Format should be: Activity Title (3 words or less, punny):, Description (3 sentences or less):.`,
-            },
-        ],
-        temperature: 0,
-          max_tokens: 500,
-          top_p: 1,
-          frequency_penalty: 0,
-          presence_penalty: 0,
-    });
-    console.log(response.data.choices[0].message);
-}
+router.post('/vacations/:id', activityCtrl.showActivity)
 
-getResponse();
+
+
 
 module.exports = router;
