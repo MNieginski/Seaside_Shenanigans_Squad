95de24548f245b7a5783d601386d9063f4a03108
laurhimms <lauriehimmel1@gmail.com>
2023-08-17 08:53:12 -0700

    make chatgpt more creative

--- controllers/activities.js
+++ controllers/activities.js
@@ -56,11 +56,11 @@ async function getResponse(req,res, next) {
                 (Activity Title): (Activity Description)`
             },
         ],
-        temperature: 0,
-          max_tokens: 500,
-          top_p: 1,
-          frequency_penalty: 0,
-          presence_penalty: 0,
+        temperature: 0.7,
+        max_tokens: 500,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
     });
     const message = response.data.choices[0].message
     const activityContent = message.content
